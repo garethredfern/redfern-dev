0920eb368e4d7aec56f3897d5fb32f9c16e6f63f
Gareth Redfern <gareth@garethredfern.com>
2020-08-25 22:22:56 +0100

    :wrench: add analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,84 +3,84 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'Articles focused on learning Laravel and VueJS',
+    title: "Articles focused on learning Laravel and VueJS",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'HandheldFriendly', content: 'True' },
-      { name: 'referrer', content: 'none-when-downgrade' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "HandheldFriendly", content: "True" },
+      { name: "referrer", content: "none-when-downgrade" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
+        hid: "description",
+        name: "description",
         content:
-          'Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.',
+          "Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.",
       },
-      { property: 'og:site_name', content: 'Redfern Dev' },
-      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { property: "og:site_name", content: "Redfern Dev" },
+      { hid: "og:type", property: "og:type", content: "website" },
       {
-        hid: 'og:url',
-        property: 'og:url',
-        content: 'https://www.redfern.dev/',
-      },
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: 'Articles focused on learning  Laravel and VueJS',
+        hid: "og:url",
+        property: "og:url",
+        content: "https://www.redfern.dev/",
       },
       {
-        hid: 'og:description',
-        property: 'og:description',
+        hid: "og:title",
+        property: "og:title",
+        content: "Articles focused on learning  Laravel and VueJS",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
         content:
-          'Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.',
+          "Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.",
       },
       {
-        hid: 'og:image',
-        property: 'og:image',
-        content: 'https://www.redfern.dev/content/images/2020/05/logo-3.png',
+        hid: "og:image",
+        property: "og:image",
+        content: "https://www.redfern.dev/content/images/2020/05/logo-3.png",
       },
-      { property: 'og:image:width', content: '740' },
-      { property: 'og:image:height', content: '300' },
-      { name: 'twitter:site', content: '@garethredfern' },
-      { name: 'twitter:card', content: 'summary_large_image' },
+      { property: "og:image:width", content: "740" },
+      { property: "og:image:height", content: "300" },
+      { name: "twitter:site", content: "@garethredfern" },
+      { name: "twitter:card", content: "summary_large_image" },
       {
-        hid: 'twitter:url',
-        name: 'twitter:url',
-        content: 'https://www.redfern.dev/',
-      },
-      {
-        hid: 'twitter:title',
-        name: 'twitter:title',
-        content: 'Articles focused on learning  Laravel and VueJS',
+        hid: "twitter:url",
+        name: "twitter:url",
+        content: "https://www.redfern.dev/",
       },
       {
-        hid: 'twitter:description',
-        name: 'twitter:description',
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "Articles focused on learning  Laravel and VueJS",
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
         content:
-          'Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.',
+          "Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.",
       },
       {
-        hid: 'twitter:image',
-        name: 'twitter:image',
-        content: 'https://www.redfern.dev/content/images/2020/05/logo-2.png',
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "https://www.redfern.dev/content/images/2020/05/logo-2.png",
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        hid: 'canonical',
-        rel: 'canonical',
-        href: 'https://www.redfern.dev/',
+        hid: "canonical",
+        rel: "canonical",
+        href: "https://www.redfern.dev/",
       },
     ],
   },
@@ -92,7 +92,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: [{ src: "~plugins/gauges.js", mode: "client" }],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
@@ -103,16 +103,16 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
+    "@nuxt/content",
   ],
   /*
    ** Content module configuration
