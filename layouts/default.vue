de7d881626c3d5dcaa4975a39ff0b2ed1d2c6387
Gareth Redfern <gareth@garethredfern.com>
2020-08-26 07:35:39 +0100

    :sparkles: add social links

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <header class="px-5 py-1 mb-5">
+    <header class="flex justify-between items-center px-5 py-1 mb-5">
       <h1>
         <a href="/" class="text-3xl uppercase">
           <span class="font-thin">redfern</span>dev
         </a>
       </h1>
+      <div class="flex">
+        <a
+          href="https://twitter.com/garethredfern"
+          target="_blank"
+          class="mx-2"
+        >
+          <Twitter />
+        </a>
+        <a href="https://github.com/garethredfern" target="_blank" class="mx-2">
+          <Github />
+        </a>
+      </div>
     </header>
     <main class="container mx-auto p-5">
       <Nuxt />
@@ -17,3 +29,12 @@
     </footer>
   </div>
 </template>
+
+<script>
+import Twitter from "@/components/svg/Twitter";
+import Github from "@/components/svg/Github";
+
+export default {
+  components: [Twitter, Github],
+};
+</script>
