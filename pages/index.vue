9bd6b3bbd98dea3604cd233a864e2160e81a34b2
Gareth Redfern <gareth@garethredfern.com>
2020-08-28 10:09:32 +0100

    :lipstick: add index styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <ul class="prose prose-lg text-gray-500 mx-auto">
+    <ul class="text-gray-500 max-w-5xl mx-auto">
       <li v-for="article of articles" :key="article.slug">
         <NuxtLink
           :to="{ name: 'articles-slug', params: { slug: article.slug } }"
+          class="md:grid gap-4 grid-cols-2 mb-12 bg-white p-5 rounded shadow"
         >
-          <img v-if="article.image" :src="article.image" alt="" />
-          <h2>{{ article.title }}</h2>
+          <img v-if="article.image" :src="article.image" alt="" class="mb-4" />
+          <div>
+            <h2 class="font-bold text-gray-900 text-2xl mb-2">
+              {{ article.title }}
+            </h2>
+            <p class="text-lg">{{ article.description }}</p>
+          </div>
         </NuxtLink>
-        <div>
-          <p>{{ article.description }}</p>
-        </div>
       </li>
     </ul>
   </div>
