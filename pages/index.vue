426f88194c709ff707074d3b14cb4b73a2cd312d
Gareth Redfern <gareth@garethredfern.com>
2020-09-01 21:49:58 +0100

    :lipstick: fix list styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <ul class="text-gray-500 max-w-5xl mx-auto">
-      <li v-for="article of articles" :key="article.slug">
+      <li
+        v-for="article of articles"
+        :key="article.slug"
+        class="md:grid md:gap-4 md:grid-cols-2 mb-12 bg-white p-5 rounded shadow"
+      >
         <NuxtLink
           :to="{ name: 'articles-slug', params: { slug: article.slug } }"
-          class="md:grid gap-4 grid-cols-2 mb-12 bg-white p-5 rounded shadow"
         >
           <img v-if="article.image" :src="article.image" alt="" class="mb-4" />
           <div>
