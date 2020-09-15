5500768bb23239475523ebae1d88f72c232c840c
Gareth Redfern <gareth@garethredfern.com>
2020-09-15 23:02:44 +0100

    update dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,6 @@ import getRoutes from "./utils/getRoutes";
 
 export default {
   /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
-  mode: "universal",
-  /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
