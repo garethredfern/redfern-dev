2aed1ce3620f32be38e512c2e8a821995b80d50c
Gareth Redfern <gareth@garethredfern.com>
2020-09-16 12:18:13 +0100

    move duplicate meta to getSiteMeta func

---
+++ utils/getSiteMeta.js
@@ -0,0 +1,62 @@
+const type = "website";
+const url = "https://redfern.dev";
+const title = "Articles focused on learning  Laravel and VueJS";
+const description =
+  "Articles focused on learning the Laravel and VueJS frameworks with some good old fashioned JavaScript thrown in.";
+const mainImage =
+  "https://res.cloudinary.com/redfern-web/image/upload/v1599839846/redfern-dev/png/redfern-logo.png";
+
+export default (meta) => {
+  return [
+    {
+      hid: "description",
+      name: "description",
+      content: (meta && meta.description) || description,
+    },
+    {
+      hid: "og:type",
+      property: "og:type",
+      content: (meta && meta.type) || type,
+    },
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: (meta && meta.url) || url,
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: (meta && meta.title) || title,
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: (meta && meta.description) || description,
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: (meta && meta.mainImage) || mainImage,
+    },
+    {
+      hid: "twitter:url",
+      name: "twitter:url",
+      content: (meta && meta.url) || url,
+    },
+    {
+      hid: "twitter:title",
+      name: "twitter:title",
+      content: (meta && meta.title) || title,
+    },
+    {
+      hid: "twitter:description",
+      name: "twitter:description",
+      content: (meta && meta.description) || description,
+    },
+    {
+      hid: "twitter:image",
+      name: "twitter:image",
+      content: (meta && meta.mainImage) || mainImage,
+    },
+  ];
+};
