c592eb97de444ae81459433d163d7c118e488927
Gareth Redfern <gareth@garethredfern.com>
2020-09-20 22:44:00 +0100

    :sparkles: add pagination component

---
+++ components/Pagination.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul class="flex justify-center">
+    <li class="mx-5"><a href="#">First</a></li>
+    <li class="mx-5"><a href="#">Prev</a></li>
+    <li class="mx-5"><a href="#">Next</a></li>
+    <li class="mx-5"><a href="#">Last</a></li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
