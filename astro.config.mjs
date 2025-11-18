85b26743c29666dd32533be1bf7c57a715bd48d3
Gareth Redfern <gareth@redfern.dev>
2025-11-18 21:28:47 +0000

    Add sitemap integration and update robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://redfern.dev",
+  integrations: [sitemap()],
 });
